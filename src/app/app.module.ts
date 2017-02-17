337efedf7b4937363fa4ededad233b49d15d5646
Jan Roman Jerzy Szczepan Cisowski Sas <JanRomanCisowski@Cisowscy.com>
2017-02-17 14:06:49 +0100

    korekta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { MaterialModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    [MaterialModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
